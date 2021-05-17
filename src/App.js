08bfdc0a8067e68f6a34cea4e14ac21024e30499
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-05-16 22:57:28 -0400

    header pages footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage';
 
 const App = (props) => {
+  const [ title, setTitle ] = useState('Tim Yehan Lee')
+  const [ headerLinks, setHeaderLinks ] = useState([
+    {htitle: 'Home', path: '/'},
+    {htitle: 'About', path:'/about'}, 
+  ])
+  const [ home, setHome ] = useState({
+    title: 'Always Learning',
+    subTitle: 'Solving problems with Technology',
+    text: 'Checkout my projects below'
+  })
+  const [ about, setAbout ] = useState('About Me')
 
   return (
-    <div>Hello from React</div>
+    <Router>
+      <Container fluid={true}>
+
+        <Navbar className='border-bottom' bg='transparent' expand='lg'>
+          <Navbar.Brand>{title}</Navbar.Brand>
+
+          <Navbar.Toggle className='border-0' ario-controls='navbar-toggle'/>
+          <Navbar.Collapse id='navbar-toggle'>
+            <Nav className='ml-auto'>
+              <Link className='nav-link' to='/'>Home</Link>
+              <Link className='nav-link' to='/about'>About</Link>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Route path='/' exact render={() => <HomePage
+          title={home.title}
+          subTitle={home.subTitle}
+          text={home.text}
+        />}/>
+
+        <Route path='/about' render={() => <AboutPage
+          title={about}
+        />}/>
+
+        <Footer />
+
+
+      </Container>
+    </Router>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = (props) => {
+    return(
+        <p>HomePage works!</p>
+    )
+}
+
+export default HomePage
